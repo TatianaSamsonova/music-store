7627e4a7eae621035581cfe7d69c6ae99cc6c3bc
TatianaSamsonova <tatiana.samsonova.dev@gmail.com>
2021-03-13 16:33:54 +0200

    Init commit

---
+++ src/main/java/com/ecom/sam/musicstorebe/MusicStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.sam.musicstorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStoreBeApplication.class, args);
+    }
+
+}
